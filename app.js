6c59b49a685e49cb9d2ba0561ef55e59c6def145
Aurelien <aurelien.emeriau@hotmail.fr>
2023-11-27 10:09:22 +0100

    commit avant push

--- app.js
+++ app.js
@@ -2,40 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 5000;
-
-// const welcome = (req, res) => {
-//     res.send("Welcome to Express");
-//   };
-  
-//   app.get("/", welcome);
-  
-//   const welcomeName = (req, res) => {
-//     res.send(`Welcome ${req.params.name}`);
-//   };
-  
-//   app.get("/users/:name", welcomeName);
-
-//   const cocktails = [
-//     {
-//       id: 1,
-//       name: "Margarita",
-//     },
-//     {
-//       id: 2,
-//       name: "Mojito",
-//     },
-//     {
-//       id: 3,
-//       name: "Cuba Libre",
-//     },
-//   ];
-  
-//   const getCocktails = (req, res) => {
-//     res.status(200).json(cocktails);
-//   };
-  
-//   app.get("/api/cocktails", getCocktails);
+// const port = 5000;
 
 const movies = [
     {
@@ -91,10 +58,12 @@ const movies = [
   
   app.get("/api/movies/:id", getId);
 
-app.listen(port, (err) => {
-    if (err) {
-      console.error('Something bad happened');
-    } else {
-      console.log(`Server is listening on ${port}`);
-    }
-  });
+  module.exports = app;
+
+// app.listen(port, (err) => {
+//     if (err) {
+//       console.error('Something bad happened');
+//     } else {
+//       console.log(`Server is listening on ${port}`);
+//     }
+//   });
